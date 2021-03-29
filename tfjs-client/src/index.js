99964e2d2d790f318899cea48ad56fd0d7dc21a4
Ryar Nyah <ryarnyah@gmail.com>
2021-03-29 22:41:18 +0200

    Add sample working client-app

---
+++ tfjs-client/src/index.js
@@ -0,0 +1,32 @@
+import * as tf from '@tensorflow/tfjs';
+import * as speechCommands from '@tensorflow-models/speech-commands';
+
+const recognizer = speechCommands.create(
+    'BROWSER_FFT',
+    null,
+    'http://localhost:8080/model/model.json',  // URL to the custom model's model.json
+    'http://localhost:8080/model/metadata.json'  // URL to the custom model's metadata.json
+);
+
+recognizer.ensureModelLoaded()
+    .then(() => {
+
+        // See the array of words that the recognizer is trained to recognize.
+        console.log(recognizer.wordLabels());
+
+        // `listen()` takes two arguments:
+        // 1. A callback function that is invoked anytime a word is recognized.
+        // 2. A configuration object with adjustable fields such a
+        //    - includeSpectrogram
+        //    - probabilityThreshold
+        //    - includeEmbedding
+        recognizer.listen(result => {
+            // - result.scores contains the probability scores that correspond to
+            //   recognizer.wordLabels().
+            // - result.spectrogram contains the spectrogram of the recognized word.
+            console.log(recognizer.wordLabels()[result.scores.indexOf(Math.max.apply(Math, result.scores))]);
+        }, {
+            includeSpectrogram: true,
+            probabilityThreshold: 0.75
+        });
+    });
---
+++ tfjs-client/webpack.config.js
@@ -0,0 +1,41 @@
+// Generated using webpack-cli http://github.com/webpack-cli
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/model', to: 'model', force: true }
+            ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+                type: 'asset',
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            "fs": false,
+            "util": false
+        },
+    }
+};
